55832771b280ab6e6916497c2fe2b9ec4e7c4495
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-06-15 16:05:40 +0900

    Update db.go

--- modules/db.go
+++ modules/db.go
@@ -2,10 +2,16 @@ package modules
 
 import (
   "github.com/google/uuid"
+  "golang.org/x/crypto/bcrypt"
 )
 
 func GenerateUUID() (newUuid string) {
   newUuidObject, _ := uuid.NewUUID()
   newUuid = newUuidObject.String()
   return newUuid
+}
+
+func Hashization(text string) (hashText string) {
+  hashText, _ = bcrypt.GenerateFromPassword([]byte(text),12)
+  return hashText
 }
