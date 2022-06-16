cfeb2a574ed3e2cf10ac5fe9252f4c7a8fead666
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-06-16 11:58:42 +0900

    Update config.go

--- modules/config.go
+++ modules/config.go
@@ -1,13 +1,26 @@
 package modules
 
 import (
+  "os"
   
+  "gopkg.in/go-ini/ini.v1"
 )
 
-func GetServerConfig() () {
-  
+func GetServerConfig() (serverConfig ServerConfig) {
+  cfg, err := ini.Load("server.conf")
+  if err != nil {
+    os.Exit(-1)
+  }
+  serverConfig = ConfigList{
+	  Port:      cfg.Section("server").Key("port").MustInt(8080),
+	  SSL:      cfg.Section("server").Key("SSL").MustBool(false),
+	  ServerCertificate:      cfg.Section("server").Key("port").String(),
+    PrivateKey: cfg.Section("server").Key("port").String(),
+    }
+  return serverConfig
 }
 
 func GetDBConfig() () {
+  
   
 }
