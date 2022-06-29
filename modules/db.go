bc9a60081999455d1542fc8aca45b2dc7468bf0a
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-06-29 09:57:52 +0900

    Update db.go

--- modules/db.go
+++ modules/db.go
@@ -5,19 +5,19 @@ import (
   "golang.org/x/crypto/bcrypt"
 )
 
-func GenerateUUID() (new_uuid string) {
-  new_uuid_object, _ := uuid.NewUUID()
-  new_uuid = new_uuid_object.String()
-  return new_uuid
+func GenerateUUID() (newUuid string) {
+  newUuidObject, _ := uuid.NewUUID()
+  newUuid = newUuidObject.String()
+  return newUuid
 }
 
-func Hashization(plain_text string) (hash_text string) {
-  hash_text, _ = bcrypt.GenerateFromPassword([]byte(plain_text),12)
-  return hash_text
+func Hashization(plainText string) (hashText string) {
+  hashText, _ = bcrypt.GenerateFromPassword([]byte(plainText),12)
+  return hashText
 }
 
-func HashComparison(hash_text string, plain_text string) (agreement bool) {
-  err := bcrypt.CompareHashAndPassword([]byte(hash_text), []byte(plain_text))
+func HashComparison(hashText string, plainText string) (agreement bool) {
+  err := bcrypt.CompareHashAndPassword([]byte(hashText), []byte(plainText))
   if err != nil {
      agreement = false
   } else {
